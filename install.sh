faeff6c0a93c93ff970bc7a59aab32acf9be643b
Pingu1n <86953591+ioscreams@users.noreply.github.com>
2022-05-09 14:20:42 +0200

    Right version of llvm

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "llvm installation done..."
 echo "Creating an alias for clang"
 
 if [ -r ~/.zshrc ]; then echo 'alias clang="~/.brew/Cellar/llvm/*/bin/clang"' >> ~/.zshrc; \
-  else echo 'alias clang="~/.brew/Cellar/llvm/13.0.0_1/bin/clang"' >> ~/.zprofile; fi
+  else echo 'alias clang="~/.brew/Cellar/llvm/13.0.1_1/bin/clang"' >> ~/.zprofile; fi
 
 echo "Created alias for clang..."
 
